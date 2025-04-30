a054e911c2cc4f16c5d00787cfe2ad666d2d6144
mehedi-hasannnn <mhasan2999@gmail.com>
2025-04-30 21:58:32 +0600

    readme added

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ const client = new MongoClient(uri, {
   
   async function run() {
     try {
-      // Connect the client to the server	(optional starting in v4.7)
+      // Connect the client to the servers (optional starting in v4.7)
       await client.connect();
   
       const equipmentCollection = client.db('sportDB').collection('equipment');
